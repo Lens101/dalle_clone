9d693b381a7263ec6841ec1006db4bf5ccf1d62e
lens101 <robertharbord@hotmail.com>
2024-02-03 14:25:10 +0000

    Get server running locally.

--- server/index.js
+++ server/index.js
@@ -9,8 +9,15 @@ const app = express();
 
 //Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+const startServer = async () => {
+  app.listen(8080, () => console.log("Server is running on port 8080"));
+};
+
+//npm start
+startServer();
